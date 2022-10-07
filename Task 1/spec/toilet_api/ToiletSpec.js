ccd95d5ce1ccffae9e217ae3e9482c3f43773935
Martynas Gelumbauskas <martynas.gelumbauskas@gmail.com>
2022-10-07 17:24:33 +0300

    Made spec js cleaner

--- Task 1/spec/toilet_api/ToiletSpec.js
+++ Task 1/spec/toilet_api/ToiletSpec.js
@@ -11,57 +11,43 @@ beforeAll(async function() {
   url = response.data.targetUrl;
 });  
 
+async function makeCallToApi(user, passwd) {
+  const response = await axios.post(url, {}, {
+    auth: {
+      username: user,
+      password: passwd
+    }
+  }).catch(function(error) {
+    return error;
+  });
+  return response;
+}
+
 describe('Toilet API Suite', function() {
   it('API call should return 401 code when no username and password provided', async function() {
-    await axios.post(url, {}, {
-      auth: {
-        username: '',
-        password: ''
-      }
-    }).catch(function(error) {
+      const error = await makeCallToApi('', '');
       expect(error.response.status).toEqual(401);
       console.log("Status code is 401 when crediantials are incorrect Verification Passed")
     });
-  });
 
   it('API call should return 200 code when username and password are provided', async function() {
-    const response = await axios.post(url, {}, {
-      auth: {
-        username: username,
-        password: password
-      }
-    }).catch(function(error) {
-      console.log(error);
-    });
+    const response = await makeCallToApi(username, password);
     expect(response.status).toEqual(200);
     console.log("Status code is 200 when crediantials are correct Verification Passed")
   });
 
   it('API call returns a valid IP address', async function() {
-    const response = await axios.post(url, {}, {
-      auth: {
-        username: username,
-        password: password
-      }
-    }).catch(function(error) {
-      console.log(error);
-    });
+    const response = await makeCallToApi(username, password);
     expect(require('net').isIPv4(response.data.ip)).toBeTrue();
     console.log("The Call returned a valid IP address verification Passed")
   });
 
   it('API call returned token that has 10 symbols', async function() {
-    const response = await axios.post(url, {}, {
-      auth: {
-        username: username,
-        password: password
-      }
-    }).catch(function(error) {
-      console.log(error);
-    });
+    const response = await makeCallToApi(username, password);
     expect(response.data.token.length).toEqual(10);
     console.log("The Call returned token with 10 symbols verification passed")
   });
+
 });
 
 afterAll(async function() { 
